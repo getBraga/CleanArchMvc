7f9de21387917f8ce73fb1828289888775f5bb6e
ysmael braga <ysmaelbraga01@gmail.com>
2025-06-05 15:50:56 -0300

    ajuste nos metodos que tem o Id como parametro

--- CleanArchMvc.Application/Interfaces/IProductService.cs
+++ CleanArchMvc.Application/Interfaces/IProductService.cs
@@ -6,8 +6,8 @@ namespace CleanArchMvc.Application.Interfaces
     public interface IProductService
     {
         Task<IEnumerable<ProductDTO>> GetProducts();
-        Task<ProductDTO> GetProductById(int? id);
-        Task<ProductDTO> GetProductCategory(int? id);
+        Task<ProductDTO> GetProductById(int id);
+        Task<ProductDTO> GetProductCategory(int id);
         Task<ProductDTO> Add(ProductDTO productDTO);
         Task<ProductDTO> Update(ProductDTO productDTO);
         Task Delete(int? id);

0136f2371b9eb01b8e0463ebbb853c851fd30a76
ysmael braga <ysmaelbraga01@gmail.com>
2025-06-13 00:17:30 -0300

    ajustes no dto porque eles estavam com o set privado

--- CleanArchMvc.Application/DTOs/ProductDTO.cs
+++ CleanArchMvc.Application/DTOs/ProductDTO.cs
@@ -8,36 +8,37 @@ namespace CleanArchMvc.Application.DTOs
 {
     public class ProductDTO
     {
-        public int Id { get; private set; }
+        public int Id { get;  set; }
         [Required(ErrorMessage = "The Name is Required")]
         [MinLength(3)]
         [MaxLength(100)]
         [DisplayName("Name")]
-        public string Name { get; private set; } = string.Empty;
+        public string Name { get;  set; } = string.Empty;
 
         [Required(ErrorMessage = "The Description is Required")]
         [MinLength(5)]
         [MaxLength(200)]
         [DisplayName("Description")]
-        public string Description { get; private set; } = string.Empty;
+        public string Description { get;  set; } = string.Empty;
         
         [Required(ErrorMessage = "The Price is Required")]
         [Column(TypeName ="decimal(18,2)")]
         [DisplayFormat(DataFormatString = "{0:C2}")]
         [DataType(DataType.Currency)]
         [DisplayName("Price")]
-        public decimal Price { get; private set; }
+        public decimal Price { get;  set; }
 
         [Required(ErrorMessage = "The Stock is Required")]
         [Range(1, 9999)]
         [DisplayName("Stock")]
-        public int Stock { get; private set; }
+        public int Stock { get;  set; }
         [MaxLength(250)]
         [DisplayName("Product Image")]
-        public string Image { get; private set; } = string.Empty;
+        public string Image { get;  set; } = string.Empty;
+        public Category? Category { get; set; }
 
         [DisplayName("Categories")]
         public int CategoryId { get; set; }
-        public Category Category { get; set; } = default!;
+      
     }
 }

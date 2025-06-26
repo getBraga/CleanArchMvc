0e8f5a59d1909caa3a415b84d36b1f84ab9a80b4
ysmael braga <ysmaelbraga01@gmail.com>
2025-06-26 11:54:32 -0300

    Feito as controllers

---
+++ CleanArchMvc.API/Controllers/CategoriesController.cs
@@ -0,0 +1,68 @@
+﻿using AutoMapper;
+using CleanArchMvc.Application.DTOs;
+using CleanArchMvc.Application.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CleanArchMvc.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoriesController : ControllerBase
+    {
+
+        private readonly ICategoryService _categoryService;
+     
+        public CategoriesController(ICategoryService categoryService)
+        {
+            _categoryService = categoryService;
+      
+        }
+        [HttpGet]   
+        public async Task<ActionResult<IEnumerable<CategoryDTO>>> Get()
+        {
+            var categories = await _categoryService.GetCategories();
+            return Ok(categories ?? []);
+        }
+        [HttpGet("{id}")]
+        public async Task<ActionResult<IEnumerable<CategoryDTO>>> Get(int id)
+        {
+            var category = await _categoryService.GetById(id);
+            if (category == null) return NotFound("Category not found!");
+            return Ok(category);
+        }
+        [HttpPost]
+        public async Task<ActionResult<CategoryDTO>> Post(CategoryDTO category) {
+
+            if (category == null) return BadRequest("Invalid Data");
+            var categoryResult = await _categoryService.Add(category);
+        
+            return Ok(categoryResult);
+        
+        }
+
+        [HttpPut]
+        public async Task<ActionResult> Put(int id, CategoryDTO category)
+        {
+            if (id != category.Id || category == null) return BadRequest();
+
+            var categoryUpdate = await _categoryService.Update(category);
+
+            if (categoryUpdate == null) return NotFound("Category not found");
+            return Ok(categoryUpdate);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> Delete(int id)
+        {
+            var category = await _categoryService.GetById(id);
+            if (category == null) return NotFound("Category not found");
+            await _categoryService.Remove(id);
+            return Ok("Category was successfully removed.");
+        }
+
+ 
+       
+
+    }
+}
---
+++ CleanArchMvc.API/Controllers/ProductsController.cs
@@ -0,0 +1,63 @@
+﻿using CleanArchMvc.Application.DTOs;
+using CleanArchMvc.Application.Interfaces;
+using CleanArchMvc.Application.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CleanArchMvc.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly IProductService _productService;
+        public ProductsController(IProductService productService)
+        {
+            _productService = productService;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<ProductDTO>>>Get()
+        {
+            var products = await _productService.GetProducts();
+            return Ok(products ?? []);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<ProductDTO>>Get(int id)
+        {
+            var product = await _productService.GetProductById(id);
+            if (product == null) return NotFound("Product not found");
+            return Ok(product);
+        }
+
+        [HttpPut]
+        public async Task<ActionResult<ProductDTO>>Put(int id, ProductDTO product)
+        {
+            if (id != product.Id) return BadRequest();
+            var result = await _productService.Update(product);
+            return Ok(result);
+
+        }
+
+        [HttpPost]
+
+        public async Task<ActionResult<ProductDTO>>Post(ProductDTO productDto)
+        {
+           if(productDto == null) return BadRequest("Invalid Data");
+            var product = await _productService.Add(productDto);
+            
+            return Ok(product);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> Delete(int id)
+        {
+            var product = await _productService.GetProductById(id);
+            if (product == null) return NotFound("Product not found");
+            await _productService.Delete(id);
+            return Ok();
+        }
+
+    }
+}

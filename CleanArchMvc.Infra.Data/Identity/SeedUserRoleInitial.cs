abcc934bf778786c2e96330747f9b8a1e9b972b2
ysmael braga <ysmaelbraga01@gmail.com>
2025-06-29 01:20:59 -0300

    retirado as senhas

--- CleanArchMvc.Infra.Data/Identity/SeedUserRoleInitial.cs
+++ CleanArchMvc.Infra.Data/Identity/SeedUserRoleInitial.cs
@@ -1,5 +1,6 @@
 ﻿using CleanArchMvc.Domain.Account;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Configuration;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,26 +13,28 @@ namespace CleanArchMvc.Infra.Data.Identity
     {
         private readonly UserManager<ApplicationUser> _userManager;
         private readonly RoleManager<IdentityRole> _roleManager;
+        private readonly IConfiguration _configuration;
 
-        public SeedUserRoleInitial(UserManager<ApplicationUser> userManager, RoleManager<IdentityRole> roleManager)
+        public SeedUserRoleInitial(UserManager<ApplicationUser> userManager, RoleManager<IdentityRole> roleManager, IConfiguration configuration)
         {
             _userManager = userManager;
             _roleManager = roleManager;
+            _configuration = configuration;
         }
         public void SeedRoles()
         {
-            if (_userManager.FindByEmailAsync("usuario@localhost").Result == null)
+            if (_userManager.FindByEmailAsync(_configuration["SeedUsers:User:Email"]?? "").Result == null)
             {
                 ApplicationUser applicationUser = new ApplicationUser();
-                applicationUser.UserName = "usuario@localhost";
-                applicationUser.Email = "usuario@localhost";
-                applicationUser.NormalizedUserName = "USUARIO@LOCALHOST";
-                applicationUser.NormalizedEmail = "USUARIO@LOCALHOST";
+                applicationUser.UserName = _configuration["SeedUsers:User:UserName"];
+                applicationUser.Email = _configuration["SeedUsers:User:Email"];
+                applicationUser.NormalizedUserName = _configuration["SeedUsers:User:NormalizedUserName"];
+                applicationUser.NormalizedEmail = _configuration["SeedUsers:User:NormalizedEmail"];
                 applicationUser.EmailConfirmed = true;
                 applicationUser.LockoutEnabled = false;
                 applicationUser.SecurityStamp = Guid.NewGuid().ToString();
 
-                IdentityResult result = _userManager.CreateAsync(applicationUser, "Numsey#2021").Result;
+                IdentityResult result = _userManager.CreateAsync(applicationUser, _configuration["SeedUsers:User:Password"] ?? "").Result;
                 if (result.Succeeded)
                 {
                     _userManager.AddToRoleAsync(applicationUser, "User").Wait();
@@ -42,15 +45,15 @@ namespace CleanArchMvc.Infra.Data.Identity
             if (_userManager.FindByEmailAsync("admin@localhost").Result == null)
             {
                 ApplicationUser applicationUser = new ApplicationUser();
-                applicationUser.UserName = "admin@localhost";
-                applicationUser.Email = "admin@localhost";
-                applicationUser.NormalizedUserName = "ADMIN@LOCALHOST";
-                applicationUser.NormalizedEmail = "ADMIN@LOCALHOST";
+                applicationUser.UserName = _configuration["SeedUsers:Admin:UserName"];
+                applicationUser.Email = _configuration["SeedUsers:Admin:Email"];
+                applicationUser.NormalizedUserName = _configuration["SeedUsers:Admin:NormalizedUserName"];
+                applicationUser.NormalizedEmail = _configuration["SeedUsers:Admin:NormalizedEmail"];
                 applicationUser.EmailConfirmed = true;
                 applicationUser.LockoutEnabled = false;
                 applicationUser.SecurityStamp = Guid.NewGuid().ToString();
 
-                IdentityResult result = _userManager.CreateAsync(applicationUser, "Numsey#2021").Result;
+                IdentityResult result = _userManager.CreateAsync(applicationUser, _configuration["SeedUsers:Admin:Password"] ?? "").Result;
                 if (result.Succeeded)
                 {
                     _userManager.AddToRoleAsync(applicationUser, "Admin").Wait();
